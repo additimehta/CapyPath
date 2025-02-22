97b027a57b4ebe703d1a0d8161bf8c3b3b4deb19
Denielle Camacho <camachodenielle@gmail.com>
2025-02-22 10:55:34 -0500

    fixed more pages stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+/*stuffs*/
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+/*components*/
 import Navbar from "./components/navbar/NavBar";
 import Chatbot from "./components/chatbot/chatBot";
-import './App.css';
-import Login from "./components/Login/Login";
-import Home from "./pages/homepage/homepage"
 
-const About = () => <h2 className="text-center mt-10">About Page</h2>;
-const Account = () => <h2 className="text-center mt-10">Account Page</h2>;
+/*pages*/
+import Login from "./components/Login/Login";
+import Home from "./pages/homepage/homepage";
+import { Account } from "./pages/accountpage/accountpage";
+import { About } from "./pages/aboutpage/aboutpage";
+
 
 function App() {
   return (
---
+++ frontend/src/pages/accountpage/accountpage.jsx
@@ -0,0 +1,7 @@
+const Account = () => {
+    return (
+        <h2 className="text-center mt-10">Account Page</h2>
+    );
+};
+
+export default Account;
