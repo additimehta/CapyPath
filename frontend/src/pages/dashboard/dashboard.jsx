78b5dad1332430c32260745ae9c393314debb9d3
Brianna Azan <brianna.azan@gmail.com>
2025-02-22 17:00:34 -0500

    card scroll

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './dashboard.css'; // Optional: Add styles for the dashboard
 
-import card1 from "../../assets/card.png";
+import card1 from "../../assets/cardDone.png";
 import card2 from "../../assets/card.png";
 import card3 from "../../assets/card.png";
+import card4 from "../../assets/card.png";
+import card5 from "../../assets/card.png";
 
 function Dashboard({ onLogout }) {
   const navigate = useNavigate();
+  const cardRefs = useRef([]);
 
   const handleLogoutClick = () => {
     navigate('/dashboard'); //change /dashboard to the path of desired nav
+  };
+
+  const scrollToCard = (index) => {
+    cardRefs.current[index].scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -21,9 +28,18 @@ function Dashboard({ onLogout }) {
         <img src={card1} className="dashboard-image" />
         <img src={card2} className="dashboard-image" />
         <img src={card3} className="dashboard-image" />
+        <img src={card4} className="dashboard-image" />
+        <img src={card5} className="dashboard-image" />
       </div>
-      <button onClick={handleLogoutClick}>Logout</button>
+      <div className="button-container">
+        <button onClick={() => scrollToCard(0)}>Card 1</button>
+        <button onClick={() => scrollToCard(1)}>Card 2</button>
+        <button onClick={() => scrollToCard(2)}>Card 3</button>
+        <button onClick={() => scrollToCard(3)}>Card 4</button>
+        <button onClick={() => scrollToCard(4)}>Card 5</button>
     </div>
+    <button onClick={handleLogoutClick}>Logout</button>
+  </div> 
   );
 }
 
