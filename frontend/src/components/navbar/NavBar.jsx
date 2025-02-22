616eb977463ba5f56b4f1397ac6781c34742c14a
Denielle Camacho <camachodenielle@gmail.com>
2025-02-22 00:40:00 -0500

    fixed merge conflict

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
-        <li><a href="/contact">Account</a></li>
         <li><a href="/login">Login </a></li>
         <li><a href="/account">Account</a></li>
       </ul>
