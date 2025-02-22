9cd4a8ef98fab90b4057ab5eb19d5b82983bbd53
Denielle Camacho <camachodenielle@gmail.com>
2025-02-21 23:15:24 -0500

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./navbar/NavBar";
 import './App.css'
+
+const Home = () => <h2 className="text-center mt-10">Home Page</h2>;
+const About = () => <h2 className="text-center mt-10">About Page</h2>;
+const Contact = () => <h2 className="text-center mt-10">Contact Page</h2>;
 
 function App() {
 
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ frontend/src/navbar/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import './NavBar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1><a href="/">CapyPath</a></h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Account</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
