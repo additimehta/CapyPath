b670b877f2233f7c9dd614884ed7b1f6d62fdb8c
additi123 <mehtaadditi@gmail.com>
2025-02-23 08:00:16 -0500

    fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import About from "./pages/aboutpage/aboutpage";
 import Dashboard from "./pages/dashboard/dashboard";
 import Form from "./components/form/form";
 import NewDashboard from "./pages/newdashboard/newdashboard";
+import CardInfo from "./pages/cardinfo/cardinfo";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/form" element={<Form />}/>
         <Route path="/dashboards" element={<NewDashboard />}/>
+        <Route path="/cardinfo" element={<CardInfo />} />
       </Routes>
       </div>
     </Router>
